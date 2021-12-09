8e8b3ba014c8cf3a36601e17f01ae27615a647e0
Dima24266 <dima24266@gmail.com>
2021-12-08 22:43:30 -0600

    1

---
+++ practika/09_functions/task_9_2.py
@@ -0,0 +1,20 @@
+trunk_mode_template = [
+"switchport mode trunk", "switchport trunk native vlan 999",
+"switchport trunk allowed vlan"
+]
+trunk_config = {
+"FastEthernet0/1": [10, 20, 30],
+"FastEthernet0/2": [11, 30],
+"FastEthernet0/4": [17]
+}
+
+def generate_access_config(access_config, access_template):
+    for i in access_config.keys():
+        restr = access_template[2]
+        access_template[2] += ' ' + str(access_config[i])
+        print('interface ', end = '')
+        print(i, end = '\n')
+        print(access_template[0], access_template[1], access_template[2].replace('[', '').replace(']', ''), sep = '\n')
+        access_template[2] = restr
+        print()
+generate_access_config(trunk_config, trunk_mode_template)
