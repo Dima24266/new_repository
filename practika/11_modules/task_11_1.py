672e4f704af03aaad2b78b5f4dacd7265933c00c
Dima24266 <dima24266@gmail.com>
2021-12-09 22:35:42 -0600

    1

---
+++ practika/11_modules/task_11_1.py
@@ -0,0 +1,30 @@
+b = input()
+b_device = b[0:2]
+d = []
+d.append(b_device)
+def parse_cdp_neighbors(command_output):
+
+    with open("sh_cdp_n_sw1.txt") as text:
+        
+        ospf = text.readlines()
+        for lines in ospf:
+            if lines.find("Eth") != -1:
+                device = lines[0:2]
+                d.append(device)
+                
+        d.sort()
+        for lines in ospf:
+            if lines.find(d[d.index(b_device) + 1]) != -1:
+                port = (lines[-8:-1])
+                local = "".join(lines.split()[1:3])
+                device = "".join(lines.split()[0])
+                eth_list = {b_device:local, device:port}
+                print(eth_list)
+            
+            elif lines.find(d[d.index(b_device) - 1]) != -1:
+                port = (lines[-8:-1])
+                local = "".join(lines.split()[1:3])
+                device = "".join(lines.split()[0])
+                eth_list = {b_device:local, device:port}
+                print(eth_list)
+parse_cdp_neighbors(b)
